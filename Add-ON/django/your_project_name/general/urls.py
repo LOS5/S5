4ef7727b0255bf621cd36e7e2a167eb1faedfa3f
LOS5 <jaisankarjaikishan4599@gmail.com>
2024-02-24 16:51:44 +0530

    login and reg

--- Add-ON/django/your_project_name/general/urls.py
+++ Add-ON/django/your_project_name/general/urls.py
@@ -6,12 +6,17 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import HomePageView, CreateFeedbackView, RegisterView
 
 from general.views import HomePageView,CreateFeedbackView
 
 urlpatterns = [
     path('home/', HomePageView.as_view(),name='index_page'),
-    path('feedback/', CreateFeedbackView.as_view(),name='feedback_page')
+    path('feedback/', CreateFeedbackView.as_view(),name='feedback_page'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register')
 ]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
--- Add-ON/django/your_project_name/general/views.py
+++ Add-ON/django/your_project_name/general/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 # Do it #3
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import TemplateView,CreateView
+from django.urls import reverse_lazy
 
 from general.models import FeedbackModel
 from general.forms import FeedbackForm
@@ -19,3 +21,8 @@ class CreateFeedbackView(CreateView):
 class HomePageView(TemplateView):
     template_name='index.html'
 # Till here #10
+
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
