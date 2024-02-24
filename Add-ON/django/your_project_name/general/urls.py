cfceac1ae28a88096dbdf22398f4b77ad47d943b
LOS5 <jaisankarjaikishan4599@gmail.com>
2024-02-24 14:49:15 +0530

    Upto02:45 in 24/02

--- Add-ON/django/your_project_name/general/urls.py
+++ Add-ON/django/your_project_name/general/urls.py
@@ -1,6 +1,6 @@
 # create this file in general manually
 
-# Do it #3
+# Do it 
 
 from django.contrib import admin
 from django.urls import path
@@ -9,12 +9,12 @@ from django.conf import settings
 
 from general.views import HomePageView,CreateFeedbackView
 
-urlpatterns = [ 
+urlpatterns = [
     path('home/', HomePageView.as_view(),name='index_page'),
-    path("feedback/",CreateFeedbackView.as_view(),name='feedback_page')
-    ]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    path('feedback/', CreateFeedbackView.as_view(),name='feedback_page')
+]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-    # Till here #18
+    # Till here
