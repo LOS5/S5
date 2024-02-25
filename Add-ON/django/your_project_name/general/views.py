a5dfdd492e79a39b880d1ca10e91af07e0bdc488
LOS5 <jaisankarjaikishan4599@gmail.com>
2024-02-25 11:10:57 +0530

    Improvement time

--- Add-ON/django/your_project_name/general/views.py
+++ Add-ON/django/your_project_name/general/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+
 
 # Do it #3
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import TemplateView,CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 
 from general.models import FeedbackModel
 from general.forms import FeedbackForm
@@ -26,3 +29,9 @@ class RegisterView(CreateView):
     template_name = 'register.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        # Automatically log in the user after successful registration
+        login(self.request, self.object)
+        return response
